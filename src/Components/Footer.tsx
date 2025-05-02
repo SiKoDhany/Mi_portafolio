1aed414ca96126776e44a88a3048f89583492e87
Daniel <Daniel11798@hotmail.com>
2025-05-02 11:53:42 -0500

    Remove scroll to top button from Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,13 +2,6 @@
 import { BsGithub, BsLinkedin, BsTwitterX } from 'react-icons/bs';
 
 const Footer = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  };
-
   return (
     <footer className="bg-[#F9F4F4] text-center py-8 px-4 border-t border-[#D4A5A5]">
       <div className="container mx-auto max-w-4xl">
@@ -45,15 +38,6 @@ const Footer = () => {
               <BsTwitterX />
             </a>
           </div>
-
-          {/* Botón de Scroll to Top */}
-          <button 
-            onClick={scrollToTop}
-            className="fixed bottom-6 right-6 bg-[#C94C4C] text-white p-3 rounded-full shadow-lg hover:bg-[#A65A4A] transition-all duration-300 ease-in-out transform hover:scale-110 z-50"
-            aria-label="Scroll to Top"
-          >
-            ↑
-          </button>
         </div>
       </div>
     </footer>
